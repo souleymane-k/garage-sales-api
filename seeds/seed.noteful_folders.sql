737685e59478ce5fcf10d52c2d5b00c6359d1a32
souleymane kone <desolo74@gmail.com>
2021-02-23 23:25:01 -0800

    restore boilerplate

--- seeds/seed.noteful_folders.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO noteful_folders (id, name)
-VALUES 
-   ('1', 'Important'),
-    ('2','Super'),
-    ('3','Spangley');
--- test/app.spec.js
+++ test/app.spec.js
@@ -3,9 +3,9 @@
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, boilerplate!"', () => {
+  it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, boilerplate!')
+      .expect(200, 'Hello, world!')
   })
 })
