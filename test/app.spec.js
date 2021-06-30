e8884da3b131b95a63ba6f095d00a0ef5c077a7e
souleymane kone <desolo74@gmail.com>
2021-06-30 11:37:22 -0700

    testing

--- test/app.spec.js
+++ test/app.spec.js
@@ -6,6 +6,6 @@ describe('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, world!')
+      .expect(200, 'Garages-Sales-Api!')
   })
 })
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,27 @@
+process.env.TZ = 'UCT'
+process.env.NODE_ENV = 'test'
+
+require('dotenv').config()
+
+process.env.TEST_DB_URL = process.env.TEST_DB_URL
+  || "postgresql://dunder_mifflin@localhost/blogful-auth-test"
+
 const { expect } = require('chai')
 const supertest = require('supertest')
 
 global.expect = expect
 global.supertest = supertest
+
+
+
+
+
+
+
+
+
+// const { expect } = require('chai')
+// const supertest = require('supertest')
+
+// global.expect = expect
+// global.supertest = supertest
